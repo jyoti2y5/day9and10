5b9b64e512318779aa538a1274da51d338bbf82a
jyoti2y5 <jyotiyadav46408@gmail.com>
2023-02-07 19:58:31 +0530

    Address book system program

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -3,8 +3,7 @@ package com.address.book;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("welcome to Address book program");
-        AddressBook addressBook = new AddressBook();
-        addressBook.addContact();
+
 
 
     }
