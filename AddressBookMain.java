0a1a1e840396a421efe5926599eabea0e8cdfe3c
jyoti2y5 <jyotiyadav46408@gmail.com>
2023-02-07 20:11:53 +0530

    create contact

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -3,6 +3,9 @@ package com.address.book;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("welcome to Address book program");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addContact();
+
 
 
 
